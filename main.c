c47b1cdea5395c0385f9de60d8876b8dcf455bfe
Alin <alin.alex1000@gmail.com>
2023-04-05 01:35:50 +0300

    command parser
    Signed-off-by: Alin <alin.alex1000@gmail.com>

--- main.c
+++ main.c
@@ -2,9 +2,65 @@
 #include <stdlib.h>
 #include <string.h>
 #include "vma.h"
+#include "utils.h"
+
+#define CMD_MAX_SIZE 300
+#define MAX_ARGC 2
+
+// reads the entire command with its argumnents from input
+void read_command(char **command)
+{
+	int cmd_len;
+	fgets(*command, sizeof(char) * CMD_MAX_SIZE, stdin);
+	// removes the '\n' character when reading the command
+	(*command)[strcspn(*command, "\n")] = 0;
+	cmd_len = strlen(*command);
+	*command = (char *)realloc(*command, cmd_len + 1);
+	// realloc will always alloc less memory (cmd_len < CMD_MAX_SIZE)
+	// so no defense needed
+}
+
+// splits the arguments into multiple one word strings
+void get_arguments(char *my_argv[MAX_ARGC], size_t *my_argc, char *data)
+{
+	int i = 0;
+	my_argv[i++] = strtok(NULL, " ");
+	while (my_argv[i - 1] && i < MAX_ARGC)
+		my_argv[i++] = strtok(NULL, " ");
+	char *rest = strtok(NULL, " ");
+	while (rest) {
+		strcat(data, rest);
+		strcat(data, " ");
+	}
+	data[strlen(data) - 1] = '\0';
+	*my_argc = i;
+}
 
 int main(void)
 {
-    
-    return 0;
+	arena_t *arena;
+	// my_argc doesn't count the command as an argument so if the input is
+	// COMMAND <arg1> <arg2>, my_argc will be 2
+	size_t my_argc = 0;
+	char *command_line, *data = NULL, *command, *my_argv[MAX_ARGC] = {NULL};
+	int exit_check = 0;
+
+	while (exit_check == 0) {
+		command_line = (char *)malloc(CMD_MAX_SIZE * sizeof(char));
+		DIE(!command_line, "Command failed!");
+		read_command(&command_line);
+		// checks if it recived only a '\n'
+		if (command_line[0] == '\0') {
+			free(command_line);
+			continue;
+		}
+		if (command_line[0] == 'W')
+		// get the main command and its args
+		command = strtok(command_line, " ");
+		get_arguments(my_argv, &my_argc, data);
+		
+		parse_command(&arena ,command, my_argv, my_argc, &exit_check, data);
+		free(command_line);
+	}
+	return 0;
 }
--- utils.h
+++ utils.h
@@ -4,16 +4,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
+#include "vma.h"
 
-/* useful macro for handling error codes */
-#define DIE(assertion, call_description)				\
-	do {								\
+/* macro for handling error codes */
+#define DIE(assertion, call_description)	\
+	do {									\
 		if (assertion) {					\
-			fprintf(stderr, "(%s, %d): ",			\
-					__FILE__, __LINE__);		\
-			perror(call_description);			\
-			exit(errno);				        \
-		}							\
+			fprintf(stderr, "(%s, %d): ",	\
+					__FILE__, __LINE__);	\
+			perror(call_description);		\
+			exit(errno);			        \
+		}									\
 	} while (0)
+
+#define INVALID_COMMAND(assertion)							\
+	do {													\
+		if (assertion) {									\
+			printf("Invalid command. Please try again.\n"); \
+			return;											\
+		}													\
+	} while(0)
+
+void parse_command(arena_t **arena,
+				   char *command, char *agrv[], size_t argc,
+				   int *exit_check,
+				   char* data);
 
 #endif /* __UTILS_H_ */
